9d1170d571b218e4350e0a0cfc0e271b35efe8aa
Einstein Bulinda <einbulinda@gmail.com>
2024-03-23 10:27:19 +0300

    Builds basic components for an application

--- main.py
+++ main.py
@@ -1,11 +1,54 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+
+class MyGridLayout(GridLayout):
+    #  Initialize infinite keywords
+    def __init__(self, **kwargs):
+        #  Call grid layout constructor
+        super(MyGridLayout, self).__init__(**kwargs)
+
+        #  Set Columns
+        self.cols = 2
+
+        #  Add Widgets
+        self.add_widget(Label(text="Name: "))
+        # Add Input Box
+        self.name = TextInput(multiline=False)
+        self.add_widget(self.name)
+
+        #  Add Widgets
+        self.add_widget(Label(text="Pizza: "))
+        # Add Input Box
+        self.pizza = TextInput(multiline=False)
+        self.add_widget(self.pizza)
+
+        #  Create a submit button
+        self.submit = Button(text="Submit", font_size=32)
+        #  Bind button for it to work
+        self.submit.bind(on_press=self.press)
+        self.add_widget(self.submit)
+
+    def press(self, instance):
+        name = self.name.text
+        pizza = self.pizza.text
+
+        # print(f'Hello {name} you like {pizza} pizza.')
+        # Print to screen
+        self.add_widget(Label(text=f'Hello {name} you like {pizza} pizza.'))
+
+        # Clear input boxes
+        self.name.text = ''
+        self.pizza.text = ''
 
 
 class MyApp(App):
     def build(self):
-        return Label(text="Hello World")
+        return MyGridLayout()
 
 
 if __name__ == '__main__':
